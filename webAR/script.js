225bbd7743ac4ca0920309c38342a7d41874351c
mmahess <mahestra@student.undiksha.ac.id>
2025-07-02 00:21:07 +0800

    Create script.js

---
+++ webAR/script.js
@@ -0,0 +1,12 @@
+AFRAME.registerComponent('tombol-link', {
+  init: function () {
+
+    this.el.addEventListener('click', (e) => {
+
+      console.log("Tombol virtual diklik!");
+
+
+      window.location.href = '../detail.html?id=mt03st1';
+    });
+  }
+});
